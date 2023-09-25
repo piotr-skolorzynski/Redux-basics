4af05b40b3be6575de88cd7286802f3b975693a0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-25 11:21:18 +0200

    work with multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,8 @@ import classes from './Counter.module.css';
 import { ACTION_TYPES } from '../store';
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter)
 
   const dispatch = useDispatch();
 
@@ -14,19 +15,19 @@ const Counter = () => {
     dispatch({ type: ACTION_TYPES.incrementByValue, payload: { value: 5 } })
   }
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => dispatch({ type: ACTION_TYPES.toggleCounterState });
 
   return (
     <main className={ classes.counter }>
       <h1>Redux Counter</h1>
-      <div className={ classes.value }>{ counter }</div>
+      { showCounter && <div className={ classes.value }>{ counter }</div> }
       <div>
         <button onClick={ incrementHandler }>Increment</button>
         <button onClick={ incrementByFiveHandler }>Increase by 5</button>
         <button onClick={ decrementHandler }>Decrement</button>
       </div>
       <button onClick={ toggleCounterHandler }>Toggle Counter</button>
-    </main>
+    </main >
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -3,23 +3,31 @@ import { createStore } from 'redux';
 export const ACTION_TYPES = {
     increment: "INCREMENT",
     decrement: "DECREMENT",
-    incrementByValue: "INCREMENT_BY_VALUE"
+    incrementByValue: "INCREMENT_BY_VALUE",
+    toggleCounterState: "TOGGLE_COUNTER_STATE"
 }
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {
+    counter: 0,
+    showCounter: true
+}
+
+const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case ACTION_TYPES.increment:
-            return { counter: state.counter += 1 };
+            return { counter: state.counter += 1, showCounter: state.showCounter };
 
         case ACTION_TYPES.decrement:
-            return { counter: state.counter -= 1 };
+            return { counter: state.counter -= 1, showCounter: state.showCounter };
 
         case ACTION_TYPES.incrementByValue:
-            return { counter: state.counter + action.payload.value };
+            return { counter: state.counter + action.payload.value, showCounter: state.showCounter };
+
+        case ACTION_TYPES.toggleCounterState:
+            return { counter: state.counter, showCounter: !state.showCounter };
 
         default:
             return state;
-
     }
 }
 
