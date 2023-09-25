5bda043879d535335f7a4f0c61d13056f1e17500
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-25 12:11:09 +0200

    replace store with slice state

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,28 @@
-import { createStore } from 'redux';
-
-export const ACTION_TYPES = {
-    increment: "INCREMENT",
-    decrement: "DECREMENT",
-    incrementByValue: "INCREMENT_BY_VALUE",
-    toggleCounterState: "TOGGLE_COUNTER_STATE"
-}
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     counter: 0,
     showCounter: true
 }
 
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ACTION_TYPES.increment:
-            return { counter: state.counter += 1, showCounter: state.showCounter };
-
-        case ACTION_TYPES.decrement:
-            return { counter: state.counter -= 1, showCounter: state.showCounter };
-
-        case ACTION_TYPES.incrementByValue:
-            return { counter: state.counter + action.payload.value, showCounter: state.showCounter };
-
-        case ACTION_TYPES.toggleCounterState:
-            return { counter: state.counter, showCounter: !state.showCounter };
-
-        default:
-            return state;
+const counter = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state, _) {
+            state.counter++;
+        },
+        decrement(state, _) {
+            state.counter--;
+        },
+        incrementByValue(state, action) {
+            state.counter = state.counter + action.payload.value;
+        },
+        toggleCounterState(state, _) {
+            state.showCounter = !state.showCounter;
+        }
     }
-}
+})
 
-const store = createStore(counterReducer);
 
-export default store;
+export default counter;
